6f4477a048301e3e4b8b82c4502fece2bf7b18c5
lqb1996 <lqb1139134758@163.com>
2019-03-08 16:12:37 +0800

    'save_db'

--- military/pipelines.py
+++ military/pipelines.py
@@ -8,8 +8,8 @@ import military.mysql_dao as py2db
 
 class MilitaryPipeline(object):
     def process_item(self, item, spider):
-        buffer = ['news_title', 'news_url', 'news_date', 'news_times', 'news_source', 'news_content']
-        value = [item['title'], item['url'][0], item['date'], item['time'], item['source'], item['content']]
+        buffer = ['news_title', 'news_url', 'news_date', 'news_times', 'news_source', 'news_content', 'news_object']
+        value = [item['title'], item['url'][0], item['date'], item['time'], item['source'], item['content'], item['object']]
         try:
             py2db.insert_field("baijiahao_mil_news", buffer, value)
         except Exception as e:
