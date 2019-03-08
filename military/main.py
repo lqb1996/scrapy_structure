541037ff221602f7bdd16acc3b8d85b49b0b7fdb
lqb1996 <lqb1139134758@163.com>
2019-03-08 10:04:45 +0800

    rm

--- military/main.py
+++
@@ -1,6 +0,0 @@
-from scrapy import cmdline
-import military.mysql_dao as pdbc
-
-
-cmdline.execute('scrapy crawl baijiahao -a parms="F22"'.split())
-# cmdline.execute('scrapy crawl baijiahao'.split())
